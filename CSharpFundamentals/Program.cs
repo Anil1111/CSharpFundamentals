40a93d8783cc2e001b5fba8a6180430791f6b779
Khanh Nguyen <con.nguyen.86@gmail.com>
2017-11-03 20:56:05 -0700

    enums, value types, and reference types

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace CSharpFundamentals
 {
+    public enum ShippingMethod
+        {
+        RegulerAirMail = 1,
+        RegisterAirMail = 2,
+        Express = 3
+        }
+
     class Program
     {
         static void Main(string[] args)
@@ -29,6 +36,44 @@ d:\folder1\folder2
 d:\folder3\folder4";
             Console.WriteLine(text2);
             //verbatim string! uses @ sign
+
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            //or we can cast the enum to shipping method instead of an integer
+            Console.WriteLine((ShippingMethod)methodId);
+            // console.writeline converts all values into ToString by default
+            Console.WriteLine(method.ToString());
+            //so the ToString above is not necessary for console.writeline
+
+            var methodName = "Express";
+            //parsing means to take the string and change it to a different type
+            var shippingMethod = (ShippingMethod) Enum.Parse(typeof(ShippingMethod), methodName);
+            //enum is a class in system namespace with method parse. parse takes in two arguments
+            //first argument is the type. typeof method is a method in system namespace
+            //we cast ShippingMethod and then assign it to a variable
+
+            //below is a demonstration on value types and reference types
+            var a = 10;
+            var b = a;
+            b++;
+            Console.WriteLine(string.Format("a:{0}, b: {1}", a, b));
+            //0 and 1 are placeholders. when we assign b, it assigns the value at a 
+            //separate location in the stack and so they are completely independent
+            // now lets see reference types
+
+            var array1 = new int[3] { 1, 2, 3 };
+            var array2 = array1;
+            array2[0] = 0;
+            //array1 points to the object in the heap. when we copy it,
+            //array2 also points to the same object. so any changes affect both
+            Console.WriteLine(string.Format("array1[0]: {0}, array2[0]: {1}", array1[0], array2[0]));
+       
+
+
+
+
         }
     }
 }
